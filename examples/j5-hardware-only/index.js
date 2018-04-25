2cb4e6eabdbdae2cf7122e4cced89088b0a152db
hxlnt <partytimehexcellent@gmail.com>
2018-04-25 21:35:32 +0000

    fix led blink

--- examples/j5-hardware-only/index.js
+++ examples/j5-hardware-only/index.js
@@ -18,8 +18,10 @@ board.on("ready", function() {
   };
   const oled = new Oled(board, five, opts);
   const font = require('oled-font-5x7');
-  let ledStatus = "off";
+  let ledStatus = "on";
   let isMonitoring = true;
+
+  led.on();
 
   // Clear OLED
   oled.fillRect(1, 1, 128, 64, 0);
@@ -27,35 +29,53 @@ board.on("ready", function() {
   // Increase LED blink rate on button press
   button.on("press", function() {
     console.log( "Button pressed" );
-    if (isMonitoring) { isMonitoring == false; }
-    else { isMonitoring == true; }
+    isMonitoring = !isMonitoring;
   });
 
   // Print temperature to console and OLED
   temperature.on("data", function(){
       let tempCelcius = this.C
       console.log("celsius: " + tempCelcius);
-      if (tempCelcius > 25  && isMonitoring) {
+      // getting too hot
+      if (tempCelcius > 25  && isMonitoring && ledStatus !== "blink") {
+        console.log("setting to blink");
+        led.blink(500);
         ledStatus = "blink";
         oled.setCursor(1, 1);
         oled.writeString(font, 1, "ALERT TEMP TOO HIGH.", 1, true, 2);
       }
-      else if (tempCelcius <= 25 && isMonitoring) {
+      // cooling down from too hot
+      else if (tempCelcius <= 25 && isMonitoring && ledStatus !== "on" ) {
+        console.log("setting to on");
+        led.stop().on();
         ledStatus = "on";
         oled.setCursor(1, 1);
-        oled.writeString(font, 1, "Temp OK.", 1, true, 2);
-      } 
-      else {
-        ledStatus = "off";
-        oled.setCursor(1, 1);
-        oled.writeString(font, 1, "System disabled.", 1, true, 2);
+        oled.writeString(font, 1, "Temp OK.            ", 1, true, 2);
       }
-
+      // turning off led 
+      else if (!isMonitoring) {
+        console.log("turn off led");
+        led.stop().off();
+        oled.setCursor(1, 1);
+        oled.writeString(font, 1, "System disabled.    ", 1, true, 2);
+      }
+  
+  //if (ledStatus == "on") { 
+  //  led.on();
+  //  console.log("On? " + ledStatus); 
+  //}
+  //else if (ledStatus == "blink") { 
+  //  led.blink(500);
+  //  console.log("Blink? " + ledStatus);
+  //}
+  //else if (ledStatus == "off") { 
+  //  led.off(); 
+  //  console.log("Off? " + ledStatus);
+  //}
+    console.log("LED Status " + ledStatus + ", Monitoring? " + isMonitoring);
   });
      
-  if (ledStatus == "on") { led.on(); }
-  else if (ledStatus == "blink") { led.blink(500); }
-  else { led.off(); }
+
 
 
 
