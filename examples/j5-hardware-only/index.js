74a196e04b5aa730ce1f8623c603663f1101a309
hxlnt <partytimehexcellent@gmail.com>
2018-04-25 13:49:18 -0700

    Added temp alert and button functionality

--- examples/j5-hardware-only/index.js
+++ examples/j5-hardware-only/index.js
@@ -4,7 +4,6 @@ const Oled = require('oled-js');
 const board = new five.Board({
   io: new Raspi()
 });
-let ledStatus = "off"
 
 board.on("ready", function() {
   const led = new five.Led("GPIO4");
@@ -19,36 +18,46 @@ board.on("ready", function() {
   };
   const oled = new Oled(board, five, opts);
   const font = require('oled-font-5x7');
-
-  // Blink LED at 500ms interval
-  //led.blink(500);
+  let ledStatus = "off";
+  let isMonitoring = true;
 
   // Clear OLED
-  oled.fillRect(1, 1, 128, 64, 1);
+  oled.fillRect(1, 1, 128, 64, 0);
+
+  // Increase LED blink rate on button press
+  button.on("press", function() {
+    console.log( "Button pressed" );
+    if (isMonitoring) { isMonitoring == false; }
+    else { isMonitoring == true; }
+  });
 
   // Print temperature to console and OLED
   temperature.on("data", function(){
       let tempCelcius = this.C
       console.log("celsius: " + tempCelcius);
-      if (tempCelcius > 25) {
-        ledStatus = "blink"
-      }
-      else {
-        led.on();
+      if (tempCelcius > 25  && isMonitoring) {
+        ledStatus = "blink";
         oled.setCursor(1, 1);
-        oled.writeString(font, 1, "Temp", 1, true, 2);
+        oled.writeString(font, 1, "ALERT TEMP TOO HIGH.", 1, true, 2);
+      }
+      else if (tempCelcius <= 25 && isMonitoring) {
+        ledStatus = "on";
+        oled.setCursor(1, 1);
+        oled.writeString(font, 1, "Temp OK.", 1, true, 2);
       } 
+      else {
+        ledStatus = "off";
+        oled.setCursor(1, 1);
+        oled.writeString(font, 1, "System disabled.", 1, true, 2);
+      }
 
   });
      
+  if (ledStatus == "on") { led.on(); }
+  else if (ledStatus == "blink") { led.blink(500); }
+  else { led.off(); }
 
-  led.blink(500);
 
-  // Increase LED blink rate on button press
-  button.on("press", function() {
-    console.log( "Button pressed" );
-    led.blink(100);
-  });
 
   
 
