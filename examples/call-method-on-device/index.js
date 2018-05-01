d86819309dcb6754884e7686155f0b6a790f5a94
Paul DeCarlo <toolboc@gmail.com>
2018-05-01 18:17:39 -0500

    Add example invoke of DirectMethod on Device

---
+++ examples/call-method-on-device/index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var Client = require('azure-iothub').Client;
+
+var connectionString = '{iothub connection string}';
+var methodName = 'write';
+var deviceId = '{devicdId{}}';
+var moduleId = '{Johnny5IoTEdgeModuleId}';
+
+const write = {
+    peripheral: {
+      name: "alarm",
+      type: "Led",
+    },
+    state: {
+      method: "on"
+    }
+  };
+
+var client = Client.fromConnectionString(connectionString);
+
+var methodParams = {
+    methodName: methodName,
+    payload: write,
+    timeoutInSeconds: 30
+};
+
+client.invokeModuleMethod(deviceId, moduleId, methodParams, function (err, result) {
+    if (err) {
+        console.error('Failed to invoke method \'' + methodName + '\': ' + err.message);
+    } else {
+        console.log(methodName + ' on ' + deviceId + ':');
+        console.log(JSON.stringify(result, null, 2));
+    }
+});
